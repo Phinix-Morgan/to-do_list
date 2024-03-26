5c15bd9685362fe7b9750534b40ed8def911e195
Phinix Morgan <137031798+Phinix-Morgan@users.noreply.github.com>
2024-03-26 17:35:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*const btnx =  document.querySelector("#btnu");
+/* const btnx =  document.querySelector("#btnu");
 btnx.addEventListener("click",() => {
   var title = document.querySelector("#title").value;
   var des = document.querySelector("#des").value;
@@ -6,6 +6,7 @@ btnx.addEventListener("click",() => {
     itemJsonArray = [];
     itemJsonArray.push([title,des]);
     localStorage.setItem('itemsJson',JSON.stringify(itemJsonArray))
+    
 
   }
   else{
@@ -13,9 +14,11 @@ btnx.addEventListener("click",() => {
     itemJsonArray = JSON.parse(itemJsonArraystr);
     itemJsonArray.push([title,des]);
     localStorage.setItem('itemsJson',JSON.stringify(itemJsonArray))
+
   }
 
       //populate the Table
+      
       let tableBody = document.querySelector("#tableBody");
       let str = "";
       itemJsonArray.forEach((element,index) => {
@@ -27,20 +30,50 @@ btnx.addEventListener("click",() => {
           </tr>`;
       });
  tableBody.innerHTML = str;
- update();
+ updateTable(itemJsonArray);
+
 
 });
 
 const Delete = (itemIndex) => {
-console.log("Delete",itemIndex);
   itemJsonArraystr = localStorage.getItem('itemsJson')
     itemJsonArray = JSON.parse(itemJsonArraystr);
 //delete itemIndex element from the array
-
 itemJsonArray.splice(itemIndex,1);
     localStorage.setItem('itemsJson',JSON.stringify(itemJsonArray));
-    update();
-};*/
+    console.log("Delete",itemIndex);
+    updateTable(itemJsonArray);
+  
+};
+
+// Function to update the table
+const updateTable = (items) => {
+    let tableBody = document.querySelector("#tableBody");
+    let str = "";
+
+    // Generate table rows for each item
+    items.forEach((element,index) => {
+        str += `<tr>
+            <th scope="row">${index +1 }</th>
+            <td>${element[0]}</td>
+            <td>${element[1]}</td>
+             <td> <button id="btn" onclick = "Delete(${index})">Delete</button> </td>
+          </tr>`;
+    });
+
+    // Update table body
+    tableBody.innerHTML = str;
+};
+
+// Function to initialize the table on page load
+const initializeTable = () => {
+    let itemJsonArray = localStorage.getItem('itemsJson') ? JSON.parse(localStorage.getItem('itemsJson')) : [];
+    updateTable(itemJsonArray);
+};
+
+// Call initializeTable function to set up the table when the page loads
+initializeTable();
+ */
 
 const addButton = document.querySelector("#btnu");
 
